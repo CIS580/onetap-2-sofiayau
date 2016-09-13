a911b0381d9d6adcdb92562a2aa7851919ee488d
sofiayau <arcsofia.yau@hotmail.com>
2016-09-12 23:31:27 -0500

    Add files via upload

--- bundle.js
+++ bundle.js
@@ -4,11 +4,13 @@
 /* Classes */
 const Game = require('./game.js');
 const Player = require('./player.js');
+const Monster = require('./monster.js');
 
 /* Global variables */
 var canvas = document.getElementById('screen');
 var game = new Game(canvas, update, render);
-var player = new Player({x: 382, y: 460})
+var player = new Player({x: 382, y: 460});
+var monster = new Monster({x:300,y:400});
 
 /**
  * @function masterLoop
@@ -31,6 +33,8 @@ masterLoop(performance.now());
  * the number of milliseconds passed since the last frame.
  */
 function update(elapsedTime) {
+  player.update(elapsedTime);
+  monster.update(elapsedTime);
 
   // TODO: Update the game objects
 }
@@ -46,9 +50,10 @@ function render(elapsedTime, ctx) {
   ctx.fillStyle = "lightblue";
   ctx.fillRect(0, 0, canvas.width, canvas.height);
   player.render(elapsedTime, ctx);
+  monster.render(elapsedTime,ctx);
 }
 
-},{"./game.js":2,"./player.js":3}],2:[function(require,module,exports){
+},{"./game.js":2,"./player.js":3,"./monster.js":4}],2:[function(require,module,exports){
 "use strict";
 
 /**
@@ -120,6 +125,9 @@ module.exports = exports = Player;
  * @param {Postition} position object specifying an x and y
  */
 function Player(position) {
+  this.state = "waiting";
+  this.frame = 0;
+  this.timer = 0;
   this.x = position.x;
   this.y = position.y;
   this.width  = 16;
@@ -132,7 +140,33 @@ function Player(position) {
  * @function updates the player object
  * {DOMHighResTimeStamp} time the elapsed time since the last frame
  */
-Player.prototype.update = function(time) {}
+ var self = this;
+ window.onmousedown = function(event){
+   if(self.state === "waiting"){ //if use this will destined to function
+     self.x = event.clientX;
+     self.state = "walking";
+   }
+ }
+
+ }
+ /**
+  * @function updates the player object
+  * {DOMHighResTimeStamp} time the elapsed time since the last frame
+  */
+ Player.prototype.update = function(elapsedTime) {
+ this.timer += elapsedTime;
+     switch(self.state)
+     case "walking":
+     this.timer++;
+     if(this.timer > 1000/16){
+           this.frame = (this.frame + 1) % 4;
+           this.timer = 0;
+     }
+
+     this.y -= 1;
+     break;
+   }
+ }
 
 /**
  * @function renders the player into the provided context
@@ -146,7 +180,62 @@ Player.prototype.render = function(time, ctx) {
     // source rectangle
     0, 0, this.width, this.height,
     // destination rectangle
-    this.x, this.y, this.width, this.height
+    this.x, this.y, 2 * this.width, 2 * this.height
+  );
+}
+
+},{}],4:[function(require,module,exports){
+"use strict";
+module.exports = exports = Monster;
+
+/**
+ * @constructor Monster
+ * Creates a new Monster object
+ * @param {Postition} position object specifying an x and y
+ */
+function Monster(position) {
+  this.state = "alive";
+  this.frame = 0;
+  this.timer = 0;
+  this.x = position.x;
+  this.y = position.y;
+  this.width  = 14;
+  this.height = 14;
+  this.spritesheet  = new Image();
+  //step
+  this.spritesheet.src = encodeURI('assets/horns/horns up.png');
+}
+
+/**
+ * @function updates the player object
+ * {DOMHighResTimeStamp} time the elapsed time since the last frame
+ */
+Monster.prototype.update = function(elapsedTime) {
+  switch(self.state){
+    case "alive":
+    this.timer++;
+    if(this.timer > 1000/160){
+      this.frame = (this.frame + 1)%(this.monstersheet.width/this.height);
+      this.timer = 0;
+        }
+        this.y -= 1;
+        break;
+  }
+}
+
+/**
+ * @function renders the player into the provided context
+ * {DOMHighResTimeStamp} time the elapsed time since the last frame
+ * {CanvasRenderingContext2D} ctx the context to render into
+ */
+Monster.prototype.render = function(time, ctx) {
+  ctx.drawImage(
+    // image
+    this.spritesheet,
+    // source rectangle
+    0, 0, this.width, this.height,
+    // destination rectangle
+    this.x, this.y, 2 * this.width, 2 * this.height
   );
 }
 
